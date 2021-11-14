4c04b909bdf8ce397229dae8901932c102a73214
Enrique-beep <contrerasenrique973@gmail.com>
2021-11-13 20:27:44 -0700

    video: useTimeAgo hook on home page

--- pages/compose/devit/index.js
+++ pages/compose/devit/index.js
@@ -1,10 +1,12 @@
+import Head from "next/head";
+
 import { useState } from "react";
 import useUser from "@hooks/useUser";
 
 import AppLayout from "@components/Applayout";
 import Button from "@components/Button";
 
-import { addDevit } from "@firebase/client";
+import { addDevit, uploadImage } from "@firebase/client";
 import { useRouter } from "next/router";
 
 const COMPOSE_STATES = {
@@ -14,11 +16,23 @@ const COMPOSE_STATES = {
   ERROR: -1,
 };
 
+const DRAG_IMAGE_STATES = {
+  ERROR: -1,
+  NONE: 0,
+  DRAG_OVER: 1,
+  UPLOADING: 2,
+  COMPLETE: 3,
+}
+
 export default function ComposeDevit() {
   const user = useUser();
   const [status, setStatus] = useState(COMPOSE_STATES.USER_NOT_KNOWN);
   const [message, setMessage] = useState("");
   const router = useRouter();
+
+  const [drag, setDrag] = useState(DRAG_IMAGE_STATES.NONE);
+  const [task, setTask] = useState(null);
+  const [imgURL, setImgURL] = useState(null);
 
   const handleChange = (e) => {
     const { value } = e.target;
@@ -43,14 +57,42 @@ export default function ComposeDevit() {
       });
   };
 
+  const handleDragEnter = (e) => {
+    e.preventDefault();
+    setDrag(DRAG_IMAGE_STATES.DRAG_OVER);
+  }
+
+  const handleDragLeave = (e) => {
+    e.preventDefault();
+    setDrag(DRAG_IMAGE_STATES.NONE);
+  }
+
+  const handleDrop = async (e) => {
+    e.preventDefault();
+    setDrag(DRAG_IMAGE_STATES.NONE);
+    const file = e.dataTransfer.files[0];
+
+    // TODO: Create the task
+
+    const task = await uploadImage(file);
+    console.log(task);
+  }
+
   const isButtonDisabled = !message.length || status === COMPOSE_STATES.LOADING;
 
   return (
     <>
+      <Head>
+        <title>Devit / Devter 🐤</title>
+      </Head>
+
       <AppLayout>
         <form onSubmit={handleSubmit}>
           <textarea
             onChange={handleChange}
+            onDragEnter={handleDragEnter}
+            onDragLeave={handleDragLeave}
+            onDrop={handleDrop}
             placeholder="What's happening?"
           ></textarea>
           <div>
@@ -64,8 +106,13 @@ export default function ComposeDevit() {
           padding: 15px;
         }
 
+        form {
+          margin: 10px;
+        }
+
         textarea {
-          border: 0;
+          border: ${drag === DRAG_IMAGE_STATES.DRAG_OVER ? '3px dashed #09f' : '3px solid transparent'};
+          border-radius: 10px;
           padding: 15px;
           font-size: 21px;
           min-height: 200px;
