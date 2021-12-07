2ced4ce040a14a9d140baf23fba8d416c9ce0ff2
Enrique Contreras <contrerasenrique973@gmail.com>
2021-12-07 15:45:34 -0700

    chore: useDateTimeFormat hook

--- components/Devit/index.js
+++ components/Devit/index.js
@@ -1,9 +1,11 @@
 import useTimeAgo from "@hooks/useTimeAgo";
 import Avatar from "@components/Avatar";
 import styles from "./styles";
+import useDateTimeFormat from "@hooks/useDateTimeFormat";
 
 export default function Devit({ uid, avatar, username, content, createdAt, img }) {
   const timeAgo = useTimeAgo(createdAt);
+  const createdAtFormated = useDateTimeFormat(createdAt)
 
   return (
     <>
@@ -15,7 +17,7 @@ export default function Devit({ uid, avatar, username, content, createdAt, img }
         <section>
           <strong>{username}</strong>
           <span> &bull; </span>
-          <span>{timeAgo}</span>
+          <time title={createdAtFormated}>{timeAgo}</time>
           <p>{content}</p>
           { img && <img src={img} /> }
         </section>
